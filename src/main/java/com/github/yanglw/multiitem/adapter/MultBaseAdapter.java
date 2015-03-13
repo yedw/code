5de457fdf614a15b8846b31f5a612dece223f26a
yedw <827330041@qq.com>
2015-03-13 17:57:14 +0800

    sdf
    sdfdsfdsfdsf

---
+++ src/main/java/com/github/yanglw/multiitem/adapter/MultBaseAdapter.java
@@ -0,0 +1,39 @@
+package com.github.yanglw.multiitem.adapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.yanglw.multiitem.internal.MultiItemIF;
+
+import java.util.List;
+
+public class MultBaseAdapter extends MyBaseAdapter<MultiItemIF>
+{
+    private Object[] mObjs;
+    private int mTypeCount;
+
+    public MultBaseAdapter(Context context, List<MultiItemIF> list, int typeCount, Object... objs)
+    {
+        super(context, list);
+        mObjs = objs;
+        mTypeCount = typeCount;
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        return mList == null ? 0 : mList.get(position).getItemType();
+    }
+
+    @Override
+    public int getViewTypeCount()
+    {
+        return mTypeCount;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        MultiItemIF bean = mList.get(position);
+        return bean.getItemView(position, convertView, parent, mInflater, mObjs);
+    }
+}
